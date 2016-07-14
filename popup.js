88c9090fb098b9dd6ba1305e7ccaab9bbd4c13c5
Sudeep Fadadu <fadadusudeep93@gmail.com>
2016-07-14 08:43:53 +0530

    display function added | TODO : include CSS style for better display

--- popup.js
+++ popup.js
@@ -1,13 +1,5 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
+
 function getCurrentSelection(callback) {
 
   chrome.tabs.executeScript( {
@@ -23,14 +15,66 @@ function getCurrentSelection(callback) {
 
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
+
+function addRow(text, img) {
+  var div = document.createElement('kata');
+
+  div.className = 'clearfix';
+
+  div.innerHTML = '<img src="' + img + '" > </img> \
+    <div> <p>' + text + ' </p> </div>';
+
+
+
+  document.getElementById('content').appendChild(div);
+}
+
+function displayData(books)
+{
+  var i = 0;
+  for(i = 0; i < books.length ; i = i + 1)
+  {
+    addRow(books[i].title,books[i].img_url);
+  }
+
+}
+
+function extractData(response)
+{
+  var books = [];
+  var numBooks = parseInt(response.getElementsByTagName(
+	"results-end")[0].textContent);
+  var numListing = (Math.min(numBooks,7));
+  if(numListing == 0){
+    return;
+  }
+  var i = 0;
+  for(i = 0; i < numListing ; i = i + 1){
+    var book = { 
+      title:"",
+      author:"",
+      rating:0.0,
+      img_url:""
+    };
+    book.title = (response.getElementsByTagName(
+	  "work")[i].getElementsByTagName("best_book")[0].getElementsByTagName(
+	    "title")[0].textContent );
+    book.img_url = (response.getElementsByTagName(
+	  "work")[i].getElementsByTagName("best_book")[0].getElementsByTagName(
+	    "small_image_url")[0].textContent );
+    book.rating = parseFloat(response.getElementsByTagName(
+	  "work")[i].getElementsByTagName("average_rating")[0].textContent);
+    book.author = (response.getElementsByTagName(
+	  "work")[i].getElementsByTagName("best_book")[0].getElementsByTagName(
+	    "author")[0].getElementsByTagName("name")[0].textContent );
+
+    books.push(book);
+  }
+//  console.log(books);
+  displayData(books);
+}
+
+function getSearchResults(searchTerm, errorCallback) {
 
   if(!searchTerm || searchTerm == ''){
     errorCallback("No Text Selected");
@@ -39,36 +83,19 @@ function getImageUrl(searchTerm, callback, errorCallback) {
   var searchUrl = 'https://www.goodreads.com/search/index.xml?' + 
     'key=5Jvii4ApD6doocrnEWm9A&q=' + searchTerm ;
   var x = new XMLHttpRequest();
-  x.open('GET', searchUrl,true);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  //var myArr = JSON.parse(x.responseText);
+  x.open('GET', searchUrl,true)
   x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    /*
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-    */
     if(!x.responseXML)
     {
       console.log("fuckoff");
       return;
     }
+    /*
     var book = x.responseXML.getElementsByTagName("work")[0].getElementsByTagName("best_book");
     console.log(book[0].getElementsByTagName("title")[0].textContent);
+    console.log(x.responseXML.getElementsByTagName("results-end")[0].textContent);
+    */
+    extractData(x.responseXML);
   };
   x.onerror = function() {
     errorCallback('Network error.');
@@ -86,21 +113,7 @@ document.addEventListener('DOMContentLoaded', function() {
     renderStatus('Performing GoodReads search for ' + selection);
      
 
-    getImageUrl(selection, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
+    getSearchResults(selection,  function(errorMessage) {
       renderStatus('Cannot display image. ' + errorMessage);
     });
   });
